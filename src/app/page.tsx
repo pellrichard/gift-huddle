c45895b1921a532f342c8319fb494d491c30f53a
pellrichard <pell_richard@hotmail.com>
2025-09-25 01:15:57 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-/* eslint-disable @next/next/no-img-element */
+'use client'\n/* eslint-disable @next/next/no-img-element */
 export default function Page() {
   return (
     <main className="min-h-screen bg-gradient-to-b from-white via-gray-50 to-gray-100 text-gray-900">
