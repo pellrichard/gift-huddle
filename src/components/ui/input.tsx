1dd14f59a84bed5c18f52ecc6349ecf9af17e288
pellrichard <pell_richard@hotmail.com>
2025-09-27 09:38:02 +0100

    Update input.tsx

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,19 +1,10 @@
 "use client";
 import * as React from "react";
-import { cn } from "@/lib/utils";
-export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+// Use a type alias to avoid @typescript-eslint/no-empty-object-type
+export type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
+
 export const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, ...props }, ref) => {
-    return (
-      <input
-        ref={ref}
-        className={cn(
-          "block w-full rounded-md border border-gray-300 px-3 py-2 text-sm shadow-sm focus:border-pink-500 focus:ring-pink-500",
-          className
-        )}
-        {...props}
-      />
-    );
-  }
+  ({ className, ...props }, ref) => <input ref={ref} className={className} {...props} />
 );
 Input.displayName = "Input";
